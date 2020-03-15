0add3d007516679e6a6147f70c73a38efa40985e
uuboyscy <aegis12321@gmail.com>
2020-03-15 14:17:59 +0800

    20200315-01

--- pttmovie_article_content.py
+++ pttmovie_article_content.py
@@ -14,18 +14,18 @@ for i in range(0, 3):
     # print(title)
     for t in title:
         print('------')
-        try:
             # print(t)
-            article_title = t.text
-            article_url = 'https://www.ptt.cc' + t['href']
-            article_res = requests.get(article_url, headers=headers)
-            article_soup = BeautifulSoup(article_res.text, 'html.parser')
-            article_content = article_soup.select('div[id="main-content"]')[0]
-            print(article_content.text.split('--')[0])
-            print(article_title)
-            print(article_url)
-        except:
-            print(t)
+        article_title = t.text
+        article_url = 'https://www.ptt.cc' + t['href']
+        article_res = requests.get(article_url, headers=headers)
+        article_soup = BeautifulSoup(article_res.text, 'html.parser')
+        article_content = article_soup \
+                        .select('div[id="main-content"]')[0] \
+                        .text \
+                        .split('--')[0]
+        print(article_content)
+        print(article_title)
+        print(article_url)
 
     last_page_url = 'https://www.ptt.cc' + soup.select('a[class="btn wide"]')[1]['href']
     url = last_page_url
